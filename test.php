89a24b1fc2b4b91461475c7be75760ad376b1e0a
luxiaosong <1145719733@qq.com>
2018-09-06 15:51:44 +0800

    陆3

--- test.php
+++ test.php
@@ -1,13 +1,1 @@
 <?php
-$mod = php_sapi_name();
-echo $mod;
-?>
-<html>
-<div style="width: 200px; height: 200px; background-color:green"  onclick="javascrit:a()">测试js代码是否能嵌入html标签</div>
-<a href="javascript:alert('链接嵌入');alert('链接嵌入');">测试链接</a>
-</html>
-<script>
- function a() {
-     alert("标准写法");
- }
-</script>
